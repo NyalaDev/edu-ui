5e1f8bb5e1e929e2160e453e0befa04ca80d4868
Khalid Elshafie <abolkog@gmail.com>
2020-07-08 22:10:57 +1000

    Add eslint and prettier config

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2018,
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true,
+  },
+  globals: {
+    ENV: true,
+  },
+  settings: {
+    react: {
+      version: '>16',
+    },
+  },
+  plugins: ['react', 'import'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'prettier',
+    'prettier/react',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/errors',
+  ],
+  rules: {
+    'react/prop-types': 'warn',
+    'no-console': 'warn',
+  },
+}
