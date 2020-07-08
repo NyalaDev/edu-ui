dc47d020fca3f1af398cde5eb4b6a78dfe51ec1a
Khalid Elshafie <abolkog@gmail.com>
2020-07-08 22:32:03 +1000

    Add layout header and footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,46 +1,23 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
+import React from 'react'
+import PropTypes from 'prop-types'
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
-import "./layout.css"
+import Header from './Header'
+import Footer from './Footer'
+import './layout.css'
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+    <div className="font-sans leading-normal tracking-normal">
+      <Header />
+
+      <div className="container w-full mx-auto pt-20">
+        <div className="w-full px-4 md:px-0 md:mt-8 mb-16 text-black-800 leading-normal">
+          {children}
+        </div>
       </div>
-    </>
+
+      <Footer />
+    </div>
   )
 }
 
