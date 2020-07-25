048f675240acec2b6ad5b48372ba3abe50fd1932
Alaa Yahia <alaay873@live.com>
2020-07-25 07:53:27 +0200

    Update navbar to display proper greeting

--- src/services/util.js
+++ src/services/util.js
@@ -1,3 +1,9 @@
+import {
+  getLocalStorage,
+  TOKEN_KEY,
+  USER_DATA_KEY,
+  isBrowser,
+} from './localStorage'
 export const getYoutubeThumbnail = url => {
   if (url && url.match(/youtube/)) {
     const id = url.match(/v=(.+)$/)[1]
@@ -5,3 +11,12 @@ export const getYoutubeThumbnail = url => {
     return `https://i.ytimg.com/vi/${id}/mqdefault.jpg`
   }
 }
+
+export const isLoggedIn = () => {
+  return getLocalStorage(TOKEN_KEY) ? true : false
+}
+
+export const getUser = () =>
+  isBrowser && window.localStorage.getItem(USER_DATA_KEY)
+    ? JSON.parse(window.localStorage.getItem(USER_DATA_KEY))
+    : {}
