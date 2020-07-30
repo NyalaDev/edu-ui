a6725c6aa2783c33c5ff33db7ff393e483bcefa7
Alaa Yahia <6881345+alaa-yahia@users.noreply.github.com>
2020-07-30 10:27:46 +0200

    Update all strings to use i18n (#15)

--- src/components/Signin/SigninForm.js
+++ src/components/Signin/SigninForm.js
@@ -9,11 +9,14 @@ import {
   USER_DATA_KEY,
   isBrowser,
 } from '../../services/localStorage'
+import { useTranslation } from 'react-i18next'
+
 const url = process.env.GATSBY_STRAPI_API_URL || 'http://localhost:8082'
 
 const SigninForm = () => {
   const [error, setError] = useState(null)
   const [success, setSuccess] = useState(null)
+  const { t } = useTranslation()
 
   const token = getLocalStorage(TOKEN_KEY)
   useEffect(() => {
@@ -51,20 +54,20 @@ const SigninForm = () => {
     <div className="container mx-auto h-full flex justify-center items-center">
       <div className="w-1/2">
         <h2 className="text-center font-bold text-gray-700 text-3xl">
-          {!success ? 'Sign in to Coder Hup' : ''}
+          {!success ? t('signIn') : ''}
         </h2>
         <div className="border-teal p-8 border-t-12 bg-white mb-6 rounded-lg shadow-lg mt-3">
           {!success && (
             <form onSubmit={formik.handleSubmit}>
               <div className="mb-4">
                 <label className="font-bold text-grey-darker block mb-2">
-                  Username or Email
+                  {t('userOrEmail')}
                 </label>
                 <input
                   {...formik.getFieldProps('identifier')}
                   type="text"
                   className="block appearance-none w-full bg-white border border-grey-light hover:border-grey px-2 py-2 rounded shadow"
-                  placeholder="Your Username or Password"
+                  placeholder={t('userPlaceholder')}
                 />
                 {formik.touched.identifier && formik.errors.identifier ? (
                   <div className="text-red-600 mt-1">
@@ -75,13 +78,13 @@ const SigninForm = () => {
 
               <div className="mb-4">
                 <label className="font-bold text-grey-darker block mb-2">
-                  Password
+                  {t('password')}
                 </label>
                 <input
                   {...formik.getFieldProps('password')}
                   type="password"
                   className="block appearance-none w-full bg-white border border-grey-light hover:border-grey px-2 py-2 rounded shadow"
-                  placeholder="Your Password"
+                  placeholder={t('passPlaceholder')}
                 />
                 {formik.touched.password && formik.errors.password ? (
                   <div className="text-red-600 mt-1">
@@ -95,22 +98,22 @@ const SigninForm = () => {
                   className="py-2 px-4 bg-gray-700 text-white rounded hover:bg-gray-600 focus:outline-none"
                   type="submit"
                 >
-                  Sign in
+                  {t('signIn')}
                 </button>
 
                 <a
                   className="no-underline inline-block align-baseline font-bold text-sm text-blue hover:text-blue-dark float-right"
                   href="#"
                 >
-                  Forgot Password?
+                  {t('forgotPass')}
                 </a>
               </div>
               <div className="text-center">
                 <h1 className="font-bold text-grey-darker block mb-2 mt-5">
-                  Or
+                  {t('or')}
                 </h1>
                 <button className="py-2 px-4 bg-gray-700 text-white text-center rounded hover:bg-gray-600 focus:outline-none mt-5 ml-4">
-                  <a href={`${url}/connect/github`}>Sign in using Github</a>
+                  <a href={`${url}/connect/github`}>{t('signUsingGithub')}</a>
                 </button>
               </div>
             </form>
@@ -119,30 +122,34 @@ const SigninForm = () => {
           {success && (
             <div className="mt-1 text-center">
               <div className="font-bold text-green-600 text-lg text-grey-darker block mb-2">
-                Signed in Successfuly !
+                {t('signed')}
               </div>
               <button className="py-2 px-4 bg-gray-700 text-white rounded hover:bg-gray-600 focus:outline-none mt-5">
-                <a href="/courses">Courses</a>
+                <a href="/courses">{t('courses')}</a>
               </button>
               <button
                 className="py-2 px-4 bg-gray-700 text-white rounded hover:bg-gray-600 focus:outline-none mt-5 ml-4"
                 type="submit"
                 onClick={clearToken}
               >
-                Sign out
+                {t('signOut')}
               </button>
             </div>
           )}
         </div>
 
         <div className="text-center">
-          <p className="text-grey-dark text-sm">
-            Don't have an account?{' '}
-            <a href="/signup" className="no-underline text-blue font-bold">
-              Create an Account
-            </a>
-            .
-          </p>
+          {!success ? (
+            <p className="text-grey-dark text-sm">
+              {`${t('noAccount')}` + '  '}
+              <a href="/signup" className="no-underline text-blue font-bold">
+                {t('createAccount')}
+              </a>
+              .
+            </p>
+          ) : (
+            ''
+          )}
         </div>
       </div>
     </div>
