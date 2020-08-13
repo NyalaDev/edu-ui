66f892db5f672cf1dce75325840282f66b39d023
Alaa Yahia <alaay873@live.com>
2020-08-13 02:17:18 +0200

    Update strings & Add toast

--- src/components/Header/UserMenu.js
+++ src/components/Header/UserMenu.js
@@ -60,7 +60,7 @@ const UserMenu = () => {
             {t('settings')}
           </Link>
           <Link
-            to="/"
+            to="#"
             className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
             role="menuitem"
           >
--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,19 +1,17 @@
 import React, { useState, useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
-import { navigate, Link } from 'gatsby'
+import { navigate } from 'gatsby'
 import { useFormik } from 'formik'
 import * as Yup from 'yup'
+import { toast } from 'react-toastify'
 import Layout from '../../components/Layout'
 import { getUser } from '../../services/util'
-
 import { getProfile, addProfile } from '../../services/api'
 
 const MyProfile = () => {
   const { t } = useTranslation()
   const user = getUser()
-  const [error, setError] = useState(null)
   const [profile, setProfile] = useState({})
-  const [isSaved, setIsSaved] = useState(false)
 
   useEffect(() => {
     if (!user) {
@@ -25,19 +23,14 @@ const MyProfile = () => {
         if (data) {
           setProfile(data)
         }
-        setError(null)
       } catch (err) {
         if (err.message.match(/(403|400)/)) {
-          setError('You are not a user')
-        } else setError('Something went wrong')
+          toast.error(t('notUser'))
+        } else toast.error(t('somethingWrong'))
       }
     }
     getProfileData()
   }, [])
-
-  /*   useEffect(() => {
-    console.log(profile)
-  }, [profile]) */
 
   const formik = useFormik({
     enableReinitialize: true,
@@ -48,20 +41,19 @@ const MyProfile = () => {
       twitter: profile.twitter || '',
     },
     validationSchema: Yup.object({
-      bio: Yup.string().min(50, 'should be at least 50'),
-      linkedin: Yup.string().url('Should be a link'),
-      github: Yup.string().url('Should be a link'),
-      twitter: Yup.string().url('Should be a link'),
+      bio: Yup.string().min(50, t('atLeast50')),
+      linkedin: Yup.string().url(t('beLink')),
+      github: Yup.string().url(t('beLink')),
+      twitter: Yup.string().url(t('beLink')),
     }),
     onSubmit: async values => {
       try {
         await addProfile(values)
-        setIsSaved(true)
-        setError(null)
+        toast.success(t('saved'))
       } catch (err) {
         if (err.message.match(/(403|400)/)) {
-          setError('You are not a user')
-        } else setError('Something went wrong')
+          toast.error(t('notUser'))
+        } else toast.error(t('somethingWrong'))
       }
     },
   })
@@ -73,129 +65,100 @@ const MyProfile = () => {
           <h2 className="text-center font-bold text-gray-700 text-3xl">
             {t('profile')}
           </h2>
-          {!error && !isSaved && (
-            <div className="border-teal p-8 border-t-12 bg-white mb-6 rounded-lg  mt-3">
-              <form onSubmit={formik.handleSubmit}>
-                <div className="mb-4">
-                  <label
-                    htmlFor="bio"
-                    className="font-bold text-grey-darker block mb-2"
-                  >
-                    {t('bio')}
-                  </label>
-                  <textarea
-                    id="bio"
-                    {...formik.getFieldProps('bio')}
-                    type="text"
-                    className="block appearance-none w-full bg-white border border-grey-light hover:border-grey px-2 py-2 rounded shadow"
-                    placeholder={t('bio')}
-                  />
-                  {formik.touched.bio && formik.errors.bio ? (
-                    <div className="text-red-600 mt-1">{formik.errors.bio}</div>
-                  ) : null}
-                </div>
-
-                <div className="mb-4">
-                  <label
-                    htmlFor="linkedin"
-                    className="font-bold text-grey-darker block mb-2"
-                  >
-                    {t('linkedin')}
-                  </label>
-                  <input
-                    id="linkedin"
-                    {...formik.getFieldProps('linkedin')}
-                    type="text"
-                    className="block appearance-none w-full bg-white border border-grey-light hover:border-grey px-2 py-2 rounded shadow"
-                    placeholder={t('linkedin')}
-                  />
-                  {formik.touched.linkedin && formik.errors.linkedin ? (
-                    <div className="text-red-600 mt-1">
-                      {formik.errors.linkedin}
-                    </div>
-                  ) : null}
-                </div>
-
-                <div className="mb-4">
-                  <label
-                    htmlFor="github"
-                    className="font-bold text-grey-darker block mb-2"
-                  >
-                    {t('github')}
-                  </label>
-                  <input
-                    id="github"
-                    {...formik.getFieldProps('github')}
-                    type="text"
-                    className="block appearance-none w-full bg-white border border-grey-light hover:border-grey px-2 py-2 rounded shadow"
-                    placeholder={t('github')}
-                  />
-                  {formik.touched.github && formik.errors.github ? (
-                    <div className="text-red-600 mt-1">
-                      {formik.errors.github}
-                    </div>
-                  ) : null}
-                </div>
-
-                <div className="mb-4">
-                  <label
-                    htmlFor="twitter"
-                    className="font-bold text-grey-darker block mb-2"
-                  >
-                    {t('twitter')}
-                  </label>
-                  <input
-                    id="twitter"
-                    {...formik.getFieldProps('twitter')}
-                    type="text"
-                    className="block appearance-none w-full bg-white border border-grey-light hover:border-grey px-2 py-2 rounded shadow"
-                    placeholder={t('twitter')}
-                  />
-                  {formik.touched.twitter && formik.errors.twitter ? (
-                    <div className="text-red-600 mt-1">
-                      {formik.errors.twitter}
-                    </div>
-                  ) : null}
-                </div>
-
-                <div className="flex items-center justify-between">
-                  <button
-                    className="py-2 px-4 bg-gray-700 text-white rounded hover:bg-gray-600 focus:outline-none"
-                    type="submit"
-                  >
-                    {t('save')}
-                  </button>
-                </div>
-              </form>
-            </div>
-          )}
-          {error === 'You are not a user' && (
-            <div>
-              <div className="text-red-600 mt-4 text-xl text-center font-bold">
-                {error}
-              </div>
-              <div className="text-xl mt-5 text-center">
-                <span>You can</span>
-                <Link
-                  href="/signin"
-                  className="px-4 py-2 ml-2 text-xl leading-5 bg-gray-700 text-white rounded hover:bg-gray-600 focus:outline-none "
-                  role="menuitem"
+          <div className="border-teal p-8 border-t-12 bg-white mb-6 rounded-lg  mt-3">
+            <form onSubmit={formik.handleSubmit}>
+              <div className="mb-4">
+                <label
+                  htmlFor="bio"
+                  className="font-bold text-grey-darker block mb-2"
                 >
-                  {t('signIn')}
-                </Link>
+                  {t('bio')}
+                </label>
+                <textarea
+                  id="bio"
+                  {...formik.getFieldProps('bio')}
+                  type="text"
+                  className="block appearance-none w-full bg-white border border-grey-light hover:border-grey px-2 py-2 rounded shadow"
+                  placeholder={t('bio')}
+                />
+                {formik.touched.bio && formik.errors.bio ? (
+                  <div className="text-red-600 mt-1">{formik.errors.bio}</div>
+                ) : null}
               </div>
-            </div>
-          )}
-          {error === 'Something went wrong' && (
-            <div className="text-red-600 mt-4 text-xl text-center font-bold">
-              {error}
-            </div>
-          )}
-          {isSaved && (
-            <div className="text-green-600 text-xl font-bold text-center mt-4">
-              {t('saved')}
-            </div>
-          )}
+
+              <div className="mb-4">
+                <label
+                  htmlFor="linkedin"
+                  className="font-bold text-grey-darker block mb-2"
+                >
+                  {t('socialLink', { provider: 'Linkedin' })}
+                </label>
+                <input
+                  id="linkedin"
+                  {...formik.getFieldProps('linkedin')}
+                  type="text"
+                  className="block appearance-none w-full bg-white border border-grey-light hover:border-grey px-2 py-2 rounded shadow"
+                  placeholder={t('socialLink', { provider: 'Linkedin' })}
+                />
+                {formik.touched.linkedin && formik.errors.linkedin ? (
+                  <div className="text-red-600 mt-1">
+                    {formik.errors.linkedin}
+                  </div>
+                ) : null}
+              </div>
+
+              <div className="mb-4">
+                <label
+                  htmlFor="github"
+                  className="font-bold text-grey-darker block mb-2"
+                >
+                  {t('socialLink', { provider: 'Github' })}
+                </label>
+                <input
+                  id="github"
+                  {...formik.getFieldProps('github')}
+                  type="text"
+                  className="block appearance-none w-full bg-white border border-grey-light hover:border-grey px-2 py-2 rounded shadow"
+                  placeholder={t('socialLink', { provider: 'Github' })}
+                />
+                {formik.touched.github && formik.errors.github ? (
+                  <div className="text-red-600 mt-1">
+                    {formik.errors.github}
+                  </div>
+                ) : null}
+              </div>
+
+              <div className="mb-4">
+                <label
+                  htmlFor="twitter"
+                  className="font-bold text-grey-darker block mb-2"
+                >
+                  {t('socialLink', { provider: 'Twitter' })}
+                </label>
+                <input
+                  id="twitter"
+                  {...formik.getFieldProps('twitter')}
+                  type="text"
+                  className="block appearance-none w-full bg-white border border-grey-light hover:border-grey px-2 py-2 rounded shadow"
+                  placeholder={t('socialLink', { provider: 'Twitter' })}
+                />
+                {formik.touched.twitter && formik.errors.twitter ? (
+                  <div className="text-red-600 mt-1">
+                    {formik.errors.twitter}
+                  </div>
+                ) : null}
+              </div>
+
+              <div className="flex items-center justify-between">
+                <button
+                  className="py-2 px-4 bg-gray-700 text-white rounded hover:bg-gray-600 focus:outline-none"
+                  type="submit"
+                >
+                  {t('save')}
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </Layout>
