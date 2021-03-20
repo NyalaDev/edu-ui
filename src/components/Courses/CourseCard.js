b18fdcb9f299ce66538d6a1ae9c34d0dc835798a
Alaa Yahia <6881345+alaa-yahia@users.noreply.github.com>
2021-03-20 07:20:56 +0200

    Make all arabic courses RTL (#51)

--- src/components/Courses/CourseCard.js
+++ src/components/Courses/CourseCard.js
@@ -29,11 +29,17 @@ const CourseCard = ({
     level,
   } = course
 
+  const isRtl = language.name === 'Arabic'
   const cardLink = forDashboard
     ? `/dashboard/manage/${slug}`
     : `/courses/${slug}`
+
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+    <div
+      className={`max-w-sm rounded overflow-hidden shadow-lg ${
+        isRtl ? 'rtl' : 'ltr'
+      }`}
+    >
       <Link to={cardLink}>
         <img className="w-full h-56" src={image} alt={title} />
       </Link>
