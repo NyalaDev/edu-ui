1f01d7197ff43482cbd7f354c5b63da1446e046c
Mozafar <kabaros@gmail.com>
2021-03-21 09:55:29 +0000

    Disable generating routing rules for gatsby on s3 (#62)
    Co-authored-by: mozafar <mozafar@nyala.dev>

--- gatsby-config.js
+++ gatsby-config.js
@@ -76,7 +76,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-s3`,
       options: {
-        bucketName: process.env.GATSBY_S3_BUCKET,
+        bucketName: process.env.GATSBY_S3_BUCKET || 's3://s3-bucket',
+        generateRoutingRules: false,
       },
     },
     `gatsby-plugin-styled-components`,
