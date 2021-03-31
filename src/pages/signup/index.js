12e9ceee5539b19dfe715bd3d508d68336e4b7fe
Alaa Yahia <alaay873@live.com>
2021-03-31 21:39:41 +0200

    Save progress to localstorage

--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -1,136 +1,14 @@
 import React from 'react'
-
-import { useTranslation, Link } from 'gatsby-plugin-react-i18next'
-
 import { graphql } from 'gatsby'
 import Layout from '../../components/Layout'
-import SocialButton from '../../components/SocialButton'
-import formEnhancer from './enhancedForm'
 import formikProps from '../../common/formik-props'
+import SignupForm from './SignupForm'
 
-const providers = ['GitHub']
-
-const Signup = ({
-  getFieldProps,
-  handleSubmit,
-  touched,
-  errors,
-  isSubmitting,
-}) => {
-  const { t } = useTranslation()
-
-  return (
-    <Layout>
-      <div className="bg-white w-full max-w-lg rounded-lg shadow-md overflow-hidden mx-auto">
-        <div className="py-4 px-6">
-          <h2 className="text-center text-gray-700 text-3xl">{t('signUp')}</h2>
-
-          <form onSubmit={handleSubmit}>
-            <div className="mt-4 w-full">
-              <label htmlFor="name" className="text-grey-darker block mb-2">
-                {t('name')}
-              </label>
-              <input
-                className="w-full mt-2 py-2 px-4 bg-gray-100 text-gray-700 border border-gray-300 rounded  block appearance-none placeholder-gray-500 focus:outline-none focus:bg-white"
-                type="text"
-                placeholder={t('name')}
-                aria-label="Name"
-                name="username"
-                {...getFieldProps('username')}
-              />
-              {touched.username && errors.username && (
-                <span>{errors.username}</span>
-              )}
-            </div>
-            <div className="mt-4 w-full">
-              <label htmlFor="email" className="text-grey-darker block mb-2">
-                {t('email')}
-              </label>
-              <input
-                className="w-full mt-2 py-2 px-4 bg-gray-100 text-gray-700 border border-gray-300 rounded  block appearance-none placeholder-gray-500 focus:outline-none focus:bg-white"
-                type="email"
-                placeholder={t('email')}
-                aria-label="email"
-                name="email"
-                {...getFieldProps('email')}
-              />
-              {touched.email && errors.email && <span>{errors.email}</span>}
-            </div>
-
-            <div className="mt-4 w-full">
-              <label htmlFor="password" className="text-grey-darker block mb-2">
-                {t('password')}
-              </label>
-              <input
-                className="w-full mt-2 py-2 px-4 bg-gray-100 text-gray-700 border border-gray-300 rounded  block appearance-none placeholder-gray-500 focus:outline-none focus:bg-white"
-                type="password"
-                placeholder={t('password')}
-                aria-label="password"
-                name="password"
-                {...getFieldProps('password')}
-              />
-              {touched.password && errors.password && (
-                <span>{errors.password}</span>
-              )}
-            </div>
-
-            <div className="mt-4 w-full">
-              <label
-                htmlFor="passwordConfirmation"
-                className="text-grey-darker block mb-2"
-              >
-                {t('passConfirm')}
-              </label>
-              <input
-                className="w-full mt-2 py-2 px-4 bg-gray-100 text-gray-700 border border-gray-300 rounded  block appearance-none placeholder-gray-500 focus:outline-none focus:bg-white"
-                type="password"
-                placeholder={t('passConfirm')}
-                aria-label="passwordConfirmation"
-                name="passwordConfirmation"
-                {...getFieldProps('passwordConfirmation')}
-              />
-              {touched.passwordConfirmation && errors.passwordConfirmation && (
-                <span>{errors.passwordConfirmation}</span>
-              )}
-            </div>
-
-            <div className="flex justify-between items-center mt-6">
-              {!isSubmitting && (
-                <button
-                  className="py-2 px-4 bg-gray-700 text-white rounded hover:bg-gray-600 focus:outline-none"
-                  type="submit"
-                >
-                  {t('signUp')}
-                </button>
-              )}
-              {isSubmitting && <span>One sec ...</span>}
-            </div>
-
-            <div className="text-center">
-              <h1 className="text-grey-darker block mb-2 mt-5">{t('or')}</h1>
-            </div>
-          </form>
-
-          <div>
-            {providers.map(provider => (
-              <SocialButton key={provider} provider={provider} />
-            ))}
-          </div>
-
-          <div className="text-center mt-6 mb-2">
-            <p className="text-grey-dark text-sm">
-              {`${t('hasAccount')} `}
-              <Link to="/signin" className="no-underline text-blue">
-                {t('signIn')}
-              </Link>
-              .
-            </p>
-          </div>
-        </div>
-      </div>
-    </Layout>
-  )
-}
+const Signup = () => (
+  <Layout>
+    <SignupForm />
+  </Layout>
+)
 
 Signup.propTypes = {
   ...formikProps,
@@ -142,4 +20,4 @@ export const query = graphql`
     }
   }
 `
-export default formEnhancer(Signup)
+export default Signup
--- src/templates/LectureView/VideoPlayer.js
+++ src/templates/LectureView/VideoPlayer.js
@@ -3,18 +3,27 @@ import ReactPlayer from 'react-player'
 import PropTypes from 'prop-types'
 import { AuthContext } from '../../contexts/AuthContext'
 import { addProfile } from '../../services/api'
+import { getLocalStorage, setLocalStorage } from '../../services/localStorage'
+import Modal from '../../components/Modal'
+import SignupForm from '../../pages/signup/SignupForm'
 
 const VideoPlayer = ({ url, lectureStrapiId, courseStrapiId }) => {
-  const DEFENITION_OF_COMPLETED = 0.8
   const { currentUser, isLoggedIn, setCurrentUser } = useContext(AuthContext)
+  const DEFENITION_OF_COMPLETED = 0.8
 
-  const lectures =
-    isLoggedIn &&
-    currentUser &&
-    currentUser.profile &&
-    currentUser.profile.completedlectures
-      ? currentUser.profile.completedlectures
-      : {}
+  let lecturesAlreadySaved
+  if (isLoggedIn) {
+    lecturesAlreadySaved =
+      isLoggedIn &&
+      currentUser &&
+      currentUser.profile &&
+      currentUser.profile.completedlectures
+        ? currentUser.profile.completedlectures
+        : {}
+  } else {
+    lecturesAlreadySaved =
+      JSON.parse(getLocalStorage('completedLectures')) || {}
+  }
 
   const updateCompletedLectures = async updatedLectures => {
     try {
@@ -26,31 +35,56 @@ const VideoPlayer = ({ url, lectureStrapiId, courseStrapiId }) => {
   }
 
   const handleProgress = state => {
-    const isCourseInProgress = lectures[courseStrapiId]
-    const isLectureCompleted =
-      lectures[courseStrapiId] &&
-      lectures[courseStrapiId].includes(lectureStrapiId)
-    if (!isCourseInProgress || !isLectureCompleted) {
-      if (state.played > DEFENITION_OF_COMPLETED) {
-        const updatedLectures = {
-          course: courseStrapiId,
-          lecture: lectureStrapiId,
+    if (state.played > DEFENITION_OF_COMPLETED) {
+      const isCourseInProgress = lecturesAlreadySaved[courseStrapiId]
+      const isLectureAlreadySaved =
+        lecturesAlreadySaved[courseStrapiId] &&
+        lecturesAlreadySaved[courseStrapiId].includes(lectureStrapiId)
+      console.log(isCourseInProgress, isLectureAlreadySaved, 'true')
+      if (!isLectureAlreadySaved) {
+        const completedCourseLectures = lecturesAlreadySaved[courseStrapiId]
+        console.log(
+          completedCourseLectures,
+          lectureStrapiId,
+          'completedCourseLectures'
+        )
+        const dataToSave = completedCourseLectures
+          ? {
+              ...lecturesAlreadySaved,
+              [courseStrapiId]: [...completedCourseLectures, lectureStrapiId],
+            }
+          : {
+              ...lecturesAlreadySaved,
+              [courseStrapiId]: [lectureStrapiId],
+            }
+        console.log(dataToSave, 'datatosave')
+        if (isLoggedIn) {
+          updateCompletedLectures(dataToSave)
+        } else {
+          setLocalStorage('completedLectures', JSON.stringify(dataToSave))
         }
-        updateCompletedLectures(updatedLectures)
       }
     }
   }
-
   return (
-    <div className="relative" style={{ paddingTop: '56.25%' }}>
-      <ReactPlayer
-        className="absolute top-0 right-0"
-        url={url}
-        width="100%"
-        height="100%"
-        controls
-        onProgress={isLoggedIn ? handleProgress : () => {}}
-      />
+    <div>
+      <div className="relative" style={{ paddingTop: '56.25%' }}>
+        <ReactPlayer
+          className="absolute top-0 right-0"
+          url={url}
+          width="100%"
+          height="100%"
+          controls
+          onProgress={handleProgress}
+        />
+      </div>
+      {false && (
+        <Modal>
+          <div>
+            <SignupForm />
+          </div>
+        </Modal>
+      )}
     </div>
   )
 }
