415489a3d33435cc29312979061a6bdbee04af0a
Alaa Yahia <alaay873@live.com>
2021-04-27 19:45:52 +0200

    Eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -37,3 +37,50 @@ module.exports = {
     'func-style': ['error', 'expression'],
   },
 }
+
+/* module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2018,
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true,
+  },
+  globals: {
+    ENV: true,
+  },
+  plugins: ['react', 'import', '@typescript-eslint'],
+  extends: [
+    'airbnb-typescript',
+    'plugin:import/typescript',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+    'no-console': 'warn',
+    'react/prop-types': ['warn', { ignore: ['children'] }],
+    'jsx-a11y/anchor-is-valid': 'off',
+    'react/jsx-filename-extension': [
+      'error',
+      { extensions: ['.js', '.jsx', 'tsx', 'ts'] },
+    ],
+    'react/jsx-props-no-spreading': 'off',
+    'import/prefer-default-export': 'off',
+    'import/extensions': 'off',
+    'jsx-a11y/no-onchange': 'warn',
+    'func-style': ['error', 'expression'],
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+  },
+} */
