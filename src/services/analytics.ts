7fe31d8eb277659ae593a7cb2b240766701f5896
mozafar <mozafar@nyala.dev>
2021-05-08 21:25:09 +0100

    Implement plausible analytics

--- src/services/analytics.ts
+++ src/services/analytics.ts
@@ -1,7 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export const sendEvent = (event: any, data = {}): void => {
-  if (typeof window !== 'undefined' && window.gtag) {
-    window.gtag('event', event, { ...data })
+declare global {
+  interface Window {
+    plausible: (event: string, data: { callback: VoidFunction } | any) => void
+  }
+}
+
+export const sendEvent = (event: string, data = {}): void => {
+  if (typeof window !== 'undefined' && window.plausible) {
+    window.plausible(event, data)
   }
 }
