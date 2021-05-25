7b4c4beb823057403df5359566f5adf8e5b09748
Alaa Yahia <alaay873@live.com>
2021-05-26 01:08:50 +0200

    Try to fix signup test

--- cypress/integration/signup.spec.ts
+++ cypress/integration/signup.spec.ts
@@ -18,7 +18,7 @@ context('signup', () => {
     cy.get('#passConfirm').type('testtest')
     cy.get('#signup').click()
 
-    cy.wait(4000)
+    cy.wait(5000)
 
     cy.get('#user-menu > .h-8').click({ force: true })
     cy.get('.origin-top-right').should('be.visible')
