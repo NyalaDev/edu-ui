b7de4c4461263e215df86431d4224e212fd5d9a7
Alaa Yahia <alaay873@live.com>
2021-05-26 01:20:21 +0200

    TRy to fix signup test 2nd try

--- cypress/integration/signup.spec.ts
+++ cypress/integration/signup.spec.ts
@@ -23,7 +23,28 @@ context('signup', () => {
     cy.get('#user-menu > .h-8').click({ force: true })
     cy.get('.origin-top-right').should('be.visible')
     cy.get('[href="/en#"]').click()
-    cy.contains('Login')
+
+    cy.contains('Login').click({ force: true })
+
+    cy.get('#identifier').type('test@test.test')
+    cy.get('#password').type('testtest{enter}')
+
+    cy.wait(2000)
+
+    cy.get('#user-menu > .h-8').click({ force: true })
+    cy.get('.origin-top-right').should('be.visible')
+    cy.get('[href="/en/profile"]').click()
+
+    cy.get('#name').clear().type('test')
+    cy.get('#bio')
+      .clear()
+      .type(
+        'testaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+      )
+    cy.get('#linkedin').clear().type('test')
+    cy.get('#github').clear().type('test')
+    cy.get('#twitter').clear().type('test')
+    cy.contains('Save').click()
   })
 })
 
