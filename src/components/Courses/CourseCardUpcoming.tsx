55c34fef8beeefce6f5e08f8046dce0cc2148fcd
mozafar <mozafar@nyala.dev>
2021-06-02 20:28:00 +0100

    Update Upcoming Course Card

--- src/components/Courses/CourseCardUpcoming.tsx
+++ src/components/Courses/CourseCardUpcoming.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { useTranslation } from 'gatsby-plugin-react-i18next'
+import { IoInformationCircle as InfoIcon } from 'react-icons/io5'
+import { Link, useTranslation } from 'gatsby-plugin-react-i18next'
 import HtmlViewer from '../HtmlViewer'
 import Badge from '../Badge'
 import CourseFeedback from './Feedback/CourseFeedback'
@@ -22,15 +23,28 @@ const CourseCardUpcoming: React.FC<Props> = ({ course }) => {
       <div className="flex-grow px-6 py-4">
         <div className="title text-xl mb-2">{title}</div>
 
-        <div className="flex flex-wrap my-2">
-          <Badge
-            text={getOriginalLanguageName(language.iso2) || language.name}
-            languageBadge={language.iso2}
-            link={`/languages/${language.name}`}
+        <div className="my-2 text-sm text-brmg-primary">
+          <div className="flex flex-wrap">
+            <InfoIcon size={20} className="opacity-60" />
+
+            <Link className="text-brmg-subtle" to={`/levels/${course.level}`}>
+              {t(`levels.${course.level || 'Intermediate'}`)}
+            </Link>
+            <span className="mx-1"> | </span>
+            <Link
+              className="text-brmg-subtle"
+              to={`/languages/${language.name}`}
+            >
+              {getOriginalLanguageName(language.iso2) || language.name}
+            </Link>
+          </div>
+        </div>
+        <div className="h-48 overflow-scroll">
+          <HtmlViewer
+            className="text-brmg-subtle text-base leading-relaxed"
+            data={description}
           />
         </div>
-
-        <HtmlViewer className="text-gray-700 text-base" data={description} />
       </div>
       <CourseFeedback course={course} type="UpcomingCourse" />
     </div>
