fd820446714859d15f07aa21e7dc79efd798d627
Alaa Yahia <6881345+alaa-yahia@users.noreply.github.com>
2021-06-07 10:31:45 -0700

    Update Twitter carousel design
    * IMplement tweeet carousel
    
    * Prettier
    
    * Twitter widget

--- src/components/LandingPage/TwitterWidget.tsx
+++ src/components/LandingPage/TwitterWidget.tsx
@@ -1,7 +1,12 @@
 import React, { useState } from 'react'
+import { useTranslation } from 'gatsby-plugin-react-i18next'
 import { TwitterTweetEmbed } from 'react-twitter-embed'
+import { AiOutlineTwitter } from 'react-icons/ai'
+import Button from '../General/Button'
 
 const TwitterWidget: React.FC = () => {
+  const { t } = useTranslation()
+
   const arr = [
     '1393942312812691461',
     '1394964427074646020',
@@ -33,38 +38,55 @@ const TwitterWidget: React.FC = () => {
   }
 
   return (
-    <div className="flex items-center justify-around h-auto p-36">
-      <button
-        type="button"
-        className="m-3 rounded-full h-12 w-12 border shadow-lg"
-        onClick={previousTweet}
-      >
-        {'<'}
-      </button>
-      {arr.map(
-        (tweetId, index) =>
-          index === firstTweetId && (
-            <div className="flex justify-center space-x-10 w-3/4">
-              <TwitterTweetEmbed
-                tweetId={arr[firstTweetId]}
-                placeholder="Loading Tweet/"
-                options={{ width: 550, cards: 'hidden' }}
-              />
-              <TwitterTweetEmbed
-                tweetId={arr[secondTweetId]}
-                placeholder="Loading Tweet"
-                options={{ width: 400, cards: 'hidden' }}
-              />
-            </div>
-          )
-      )}
-      <button
-        type="button"
-        className="m-3 rounded-full h-12 w-12 border shadow-lg"
-        onClick={nextTweet}
-      >
-        {'>'}
-      </button>
+    <div>
+      <div className="flex items-center justify-around h-auto pt-36 pb-8">
+        <button
+          type="button"
+          className="m-3 rounded-full h-12 w-12 border shadow-lg"
+          onClick={previousTweet}
+        >
+          {'<'}
+        </button>
+        {arr.map(
+          (tweetId, index) =>
+            index === firstTweetId && (
+              <div className="flex justify-center space-x-10 w-3/4">
+                <TwitterTweetEmbed
+                  tweetId={arr[firstTweetId]}
+                  placeholder="Loading Tweet/"
+                  options={{ width: 550, cards: 'hidden' }}
+                />
+                <TwitterTweetEmbed
+                  tweetId={arr[secondTweetId]}
+                  placeholder="Loading Tweet"
+                  options={{ width: 400, cards: 'hidden' }}
+                />
+              </div>
+            )
+        )}
+        <button
+          type="button"
+          className="m-3 rounded-full h-12 w-12 border shadow-lg"
+          onClick={nextTweet}
+        >
+          {'>'}
+        </button>
+      </div>
+
+      <div className="flex justify-center pb-24">
+        <div>
+          <Button
+            link
+            mode="info"
+            href="https://twitter.com/BarmagaIO"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <AiOutlineTwitter size="32" className="ml-2" />
+            {t('landingPage.followOnTwitter')}
+          </Button>
+        </div>
+      </div>
     </div>
   )
 }
