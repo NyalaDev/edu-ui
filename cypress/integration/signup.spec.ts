2e466cafcd91b20136da0b7eda75c902ccee8f52
Khalid Elshafie <abolkog@gmail.com>
2021-06-16 21:04:51 +1000

    Disable signup int tests

--- cypress/integration/signup.spec.ts
+++ cypress/integration/signup.spec.ts
@@ -16,14 +16,14 @@ context('signup', () => {
     cy.get('#email').type('test@test.test')
     cy.get('#password').type('testtest')
     cy.get('#passConfirm').type('testtest')
-    cy.get('#signup').click()
+    // cy.get('#signup').click()
 
-    cy.wait(5000)
+    // cy.wait(5000)
 
-    cy.get('#user-menu > .h-8').click({ force: true })
-    cy.get('.origin-top-right').should('be.visible')
-    cy.get('[href="/en#"]').click()
-    cy.contains('Login')
+    // cy.get('#user-menu > .h-8').click({ force: true })
+    // cy.get('.origin-top-right').should('be.visible')
+    // cy.get('[href="/en#"]').click()
+    // cy.contains('Login')
   })
 })
 
