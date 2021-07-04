b1567b776b2896c47c2999c41f47e9e7cec385b8
Alaa Yahia <alaay873@live.com>
2021-07-04 18:41:54 +0200

    Make change to test

--- playwright/signin_signup.spec.ts
+++ playwright/signin_signup.spec.ts
@@ -2,16 +2,7 @@ import { test, expect } from '@playwright/test'
 
 test('test', async ({ page }) => {
   // Go to https://edu.nyaladev.com/
-  await page.goto('https://edu.nyaladev.com/')
-
-  // Click [data-testid="en"]
-  await Promise.all([
-    page.waitForNavigation({ url: 'https://edu.nyaladev.com/en/' }),
-    page.click('[data-testid="en"]'),
-  ])
-
-  // Click text=Login
-  await page.click('text=Login')
+  await page.goto('https://edu.nyaladev.com/en/signin')
 
   // Click text=Create an Account
   await page.click('text=Create an Account')
@@ -20,13 +11,13 @@ test('test', async ({ page }) => {
   await page.click('[placeholder="Name"]')
 
   // Fill [placeholder="Name"]
-  await page.fill('[placeholder="Name"]', 'test testee')
+  await page.fill('[placeholder="Name"]', 'test test')
 
   // Click [placeholder="Email"]
   await page.click('[placeholder="Email"]')
 
   // Fill [placeholder="Email"]
-  await page.fill('[placeholder="Email"]', 'testtestee@testee.com')
+  await page.fill('[placeholder="Email"]', 'testee@testee.com')
 
   // Click [placeholder="Password"]
   await page.click('[placeholder="Password"]')
@@ -44,33 +35,6 @@ test('test', async ({ page }) => {
   await Promise.all([
     page.waitForNavigation({ url: 'https://edu.nyaladev.com/profile' }),
     page.click('button:has-text("Sign Up")'),
-  ])
-
-  // Click [aria-label="User menu"]
-  await page.click('[aria-label="User menu"]')
-
-  // Click text=Sign out
-  await page.click('text=Sign out')
-
-  // Click text=Login
-  await page.click('text=Login')
-
-  // Click [placeholder="Username or Email"]
-  await page.click('[placeholder="Username or Email"]')
-
-  // Fill [placeholder="Username or Email"]
-  await page.fill('[placeholder="Username or Email"]', 'testtestee@testee.com')
-
-  // Click [placeholder="Your Password"]
-  await page.click('[placeholder="Your Password"]')
-
-  // Fill [placeholder="Your Password"]
-  await page.fill('[placeholder="Your Password"]', 'testtest')
-
-  // Click button:has-text("Login")
-  await Promise.all([
-    page.waitForNavigation({ url: 'https://edu.nyaladev.com' }),
-    page.click('button:has-text("Login")'),
   ])
 
   // Click [aria-label="User menu"]
