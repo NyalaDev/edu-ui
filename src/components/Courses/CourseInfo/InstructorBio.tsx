b93276be9909de089a06eca5b0503bb7b9f68656
sami <sami@nyala.dev>
2021-09-24 09:59:33 +0100

    Show social icons for instructor

--- src/components/Courses/CourseInfo/InstructorBio.tsx
+++ src/components/Courses/CourseInfo/InstructorBio.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import { useTranslation, Link } from 'gatsby-plugin-react-i18next'
+import {
+  AiOutlineTwitter,
+  AiOutlineLinkedin,
+  AiOutlineGithub,
+} from 'react-icons/ai'
 import { User } from '../../../types/api.types'
 import Card from '../../General/Card'
 
@@ -27,6 +32,36 @@ const InstructorBio: React.FC<Props> = ({ instructor, photo }) => {
           </Link>
 
           <div className="px-2 text-sm font-bold">{instructor.profile.bio}</div>
+
+          <div className="mt-4 flex justify-center">
+            {instructor.profile.twitter && (
+              <a
+                rel="noreferrer"
+                target="_blank"
+                href={`https://twitter.com/${instructor.profile.twitter}`}
+              >
+                <AiOutlineTwitter size="32" className="ml-2" />
+              </a>
+            )}
+            {instructor.profile.linkedin && (
+              <a
+                rel="noreferrer"
+                target="_blank"
+                href={`https://linkedin.com/in/${instructor.profile.linkedin}`}
+              >
+                <AiOutlineLinkedin size="32" className="ml-2" />
+              </a>
+            )}
+            {instructor.profile.github && (
+              <a
+                rel="noreferrer"
+                target="_blank"
+                href={`https://github.com/${instructor.profile.github}`}
+              >
+                <AiOutlineGithub size="32" className="ml-2" />
+              </a>
+            )}
+          </div>
         </div>
       </div>
     </Card>
